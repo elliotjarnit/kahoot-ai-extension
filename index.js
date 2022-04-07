a8770488f55d1a17859c8a8a84df31122100a082
Swiftzerr <swiftzermc@gmail.com>
2022-04-07 10:55:52 -0400

    Got the questions

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const observer = new MutationObserver((mutations, obs) => {
     const questionTitle = document.querySelector("[data-functional-selector='block-title']");
     if (questionTitle) {
         console.log(questionTitle.innerHTML)
+        const question1 = document.querySelector("[data-functional-selector='answer-0']");
+        const question2 = document.querySelector("[data-functional-selector='answer-1']");
+        const question3 = document.querySelector("[data-functional-selector='answer-2']");
+        const question4 = document.querySelector("[data-functional-selector='answer-3']");
+        console.log(question1.lastElementChild.firstElementChild.innerHTML)
+        console.log(question2.lastElementChild.firstElementChild.innerHTML)
+        console.log(question3.lastElementChild.firstElementChild.innerHTML)
+        console.log(question4.lastElementChild.firstElementChild.innerHTML)
         obs.disconnect();
         return;
     }
