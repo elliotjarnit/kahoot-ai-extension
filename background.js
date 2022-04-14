c7b8b85c176fa3a95bdf6ec0d71f1ac7f83d99f8
Swiftzerr <swiftzermc@gmail.com>
2022-04-14 18:21:51 -0400

    Why tf does chrome need manifest 3?

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
+// If the key value does not exist in the storage, it will be created.
+// This only happens the first time the extension is installed.
+chrome.storage.local.get('key', value => {
+    if (typeof value.key === undefined) {
+        chrome.storage.local.set({key: ""});
+    }
+});
+
 // This will handle messages send from the content script
 chrome.runtime.onMessage.addListener(
     function(message, sender, sendResponse) {
--- background_scripts/storage.js
+++
@@ -1,7 +0,0 @@
-// If the key value does not exist in the storage, it will be created.
-// This only happens the first time the extension is installed.
-chrome.storage.local.get('key', value => {
-    if (typeof value.key === undefined) {
-        chrome.storage.local.set({key: ""});
-    }
-});
